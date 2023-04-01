2bc7a9b2ef619a27aa930c773ea139c238215808
Andy Chen <chen.andy18@gmail.com>
2023-04-01 14:22:07 -0700

    Time: 89 ms (5.09%), Space: 43.6 MB (24.85%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.ts
+++ 0058-length-of-last-word/0058-length-of-last-word.ts
@@ -1,10 +1,21 @@
+// /*
+// - remove fix all spacing with 1 space using regex
+// - split the string by space
+// - return length of last word
+// */
+// function lengthOfLastWord(s: string): number {
+//     const arr : Array<string> = s.replace(/^\s+|\s+$/g,'').split(' ');
+//     return arr[arr.length - 1].length;
+    
+// };
+
 /*
-- remove fix all spacing with 1 space using regex
+- remove only spacing from the sides
 - split the string by space
 - return length of last word
 */
 function lengthOfLastWord(s: string): number {
-    const arr : Array<string> = s.replace(/^\s+|\s+$/g,'').split(' ');
+    const arr : Array<string> = s.trim().split(' ');
     return arr[arr.length - 1].length;
     
 };
