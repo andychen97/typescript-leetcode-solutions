38d10c8aad8cff3d91ccfc7f494c6d3a7e9701c0
Andy Chen <chen.andy18@gmail.com>
2023-04-01 14:23:48 -0700

    Time: 64 ms (59.67%), Space: 44.2 MB (16.29%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.ts
+++ 0058-length-of-last-word/0058-length-of-last-word.ts
@@ -1,25 +1,3 @@
-// /*
-// - remove fix all spacing with 1 space using regex
-// - split the string by space
-// - return length of last word
-// */
-// function lengthOfLastWord(s: string): number {
-//     const arr : Array<string> = s.replace(/^\s+|\s+$/g,'').split(' ');
-//     return arr[arr.length - 1].length;
-    
-// };
-
-// /*
-// - remove only spacing from the sides
-// - split the string by space
-// - return length of last word
-// */
-// function lengthOfLastWord(s: string): number {
-//     const arr : Array<string> = s.trim().split(' ');
-//     return arr[arr.length - 1].length;
-    
-// };
-
 /*
 - remove only spacing from only the end
 - split the string by space
@@ -27,6 +5,28 @@
 */
 function lengthOfLastWord(s: string): number {
     const arr : Array<string> = s.trimEnd().split(' ');
+    return arr[arr.length - 1].length;
+    
+};
+
+/*
+- remove fix all spacing with 1 space using regex
+- split the string by space
+- return length of last word
+*/
+function lengthOfLastWord2(s: string): number {
+    const arr : Array<string> = s.replace(/^\s+|\s+$/g,'').split(' ');
+    return arr[arr.length - 1].length;
+    
+};
+
+/*
+- remove only spacing from the sides
+- split the string by space
+- return length of last word
+*/
+function lengthOfLastWord3(s: string): number {
+    const arr : Array<string> = s.trim().split(' ');
     return arr[arr.length - 1].length;
     
 };
